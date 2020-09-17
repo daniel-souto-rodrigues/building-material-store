c00a5c3cf4a07287771857c9c699150ba1f52363
Daniel Rodrigues <dantdmc210@gmail.com>
2020-09-17 16:05:04 -0300

    first commit

---
+++ BMS.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BMS.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
